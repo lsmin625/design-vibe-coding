3464a359a02e68688e0ef2409a2cf3167b971237
이상민(SKAX) <sm.lee1@sk.com>
2025-10-28 07:54:31 +0900

    비어있는 화면으로 구성

--- project/skax-vibe-vue/src/main.js
+++ project/skax-vibe-vue/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
