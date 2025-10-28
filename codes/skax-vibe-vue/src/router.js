5792aa28e7a23b4f76fe1bac21fa07ddc55d682d
이상민(SKAX) <sm.lee1@sk.com>
2025-10-28 14:34:38 +0900

    프로덕트 목록 화면 구성

--- codes/skax-vibe-vue/src/router.js
+++ codes/skax-vibe-vue/src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router"
 
 import Signup from "./pages/Signup.vue";
+import ProductList from "./pages/ProductList.vue";
 
 const routes = [
     { path: "/", component: Signup },
     { path: "/signup", component: Signup },
+    { path: "/products", component: ProductList },
     { path: "/:pathMatch(.*)*", redirect: "/" },
 ]
 
